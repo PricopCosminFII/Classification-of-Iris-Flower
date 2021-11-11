969d50dce7af90fdb4812bc9e101b0481e15f3d0
PaladiAlexandru <alex_paladi@yahoo.com>
2021-11-11 20:24:12 +0200

    Adding project structure

---
+++ Domain/Entities/IrisFlower.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Common;
+namespace Domain.Entities
+{
+    public class IrisFlower : BaseEntity
+    {
+        public string Class { get; set; }
+        public decimal SepalLength { get; set; }
+        public decimal SepalWidth { get; set; }
+        public decimal PetalLength { get; set; }
+        public decimal PetalWidth { get; set; }
+        
+    }
+}
---
+++ Persistence/PersistenceDI.cs
@@ -0,0 +1,22 @@
+﻿using Application.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Persistence.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence
+{
+    public static class PersistenceDI
+    {
+        public static void AddPersistence(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<IrisFlowerContext>(options => options.UseSqlServer(configuration.GetConnectionString("My connection"), b => b.MigrationsAssembly(typeof(IrisFlowerContext).Assembly.FullName)));
+            services.AddScoped<IApplicationContext>(provider => provider.GetService<IrisFlowerContext>());
+        }
+    }
+}
