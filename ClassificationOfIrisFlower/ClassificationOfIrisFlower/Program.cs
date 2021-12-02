99874caf671c3b605affc29677bee2847c3e2fbd
PricopCosminFII <79044611+PricopCosminFII@users.noreply.github.com>
2021-12-03 01:06:31 +0200

    Ml part

---
+++ ClassificationOfIrisFlower/ClassificationOfIrisFlower/Program.cs
@@ -0,0 +1,12 @@
+﻿
+using ClassificationOfIrisFlower;
+
+IrisData flower=new IrisData()
+{
+    SepalLength = 5.4f,
+    SepalWidth = 3.5f,
+    PetalLength = 1.2f,
+    PetalWidth = 0.4f,
+};
+IrisFlowerClassifier classifier = new IrisFlowerClassifier();
+Console.WriteLine(classifier.Prediction(flower));
