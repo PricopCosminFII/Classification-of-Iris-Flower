25db7988b94a78e4f8a23721c087f3659735b395
PricopCosminFII <79044611+PricopCosminFII@users.noreply.github.com>
2021-12-03 01:03:46 +0200

    ML part

---
+++ ClassificationOfIrisFlower/ClassificationOfIrisFlower/IrisFlowerClassifier.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.ML;
+
+
+namespace ClassificationOfIrisFlower
+{
+    public class IrisFlowerClassifier
+    {public string Prediction(IrisData flower)
+        {
+            MLContext mlContext = new MLContext();
+            IDataView trainingDataView = mlContext.Data.LoadFromTextFile<IrisData>("iris-data.txt", separatorChar: ',', hasHeader: true); ;
+            var pipeline = mlContext.Transforms.Conversion.MapValueToKey("Label")
+               .Append(mlContext.Transforms.Concatenate("Features", "SepalLength", "SepalWidth", "PetalLength", "PetalWidth"))
+               .AppendCacheCheckpoint(mlContext)
+               .Append(mlContext.MulticlassClassification.Trainers.SdcaMaximumEntropy(labelColumnName: "Label", featureColumnName: "Features"))
+               .Append(mlContext.Transforms.Conversion.MapKeyToValue("PredictedLabel"));
+            var model = pipeline.Fit(trainingDataView);
+            var prediction = mlContext.Model.CreatePredictionEngine<IrisData, IrisPrediction>(model).Predict(flower);
+             
+            return prediction.PredictedLabels;
+        }
+    }
+}
---
+++ ClassificationOfIrisFlower/ClassificationOfIrisFlower/IrisPrediction.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.ML.Data;
+
+namespace ClassificationOfIrisFlower
+{
+    internal class IrisPrediction
+    {
+        [ColumnName("PredictedLabel")]
+        public string PredictedLabels { get; set; }
+    }
+}
